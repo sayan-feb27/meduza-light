bcc25e89d043415357af62d244a08465e54d6e50
Saian Badarchy <sayan.badarchy@icloud.com>
2022-02-04 13:58:27 +0300

    disable browsable rest api for prod update postgres healthcheck interval

--- meduza_light/settings/production.py
+++ meduza_light/settings/production.py
@@ -2,3 +2,7 @@ from .base import *
 
 DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
+
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",)
+}
